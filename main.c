4f5edac60481e82bae1b6314b623b771477ce839
Eivind Fonn <evfonn@gmail.com>
2016-06-04 20:24:29 +0200

    Support variable sized mpfs

--- main.c
+++ main.c
@@ -6,54 +6,68 @@
 #include <mpi.h>
 
 #define PREC 100
-#define NLIMBS \
-    ((mp_size_t) ((__GMP_MAX (53, PREC) + 2 * GMP_NUMB_BITS - 1) / GMP_NUMB_BITS))
-#define PACKED_LIMBS ((size_t) NLIMBS + 3)
-#define PACKED_BYTES ((size_t) PACKED_LIMBS * sizeof(mp_limb_t))
 
-mp_limb_t *pack(mp_limb_t *dest, mpf_t* src, int n)
+#define NLIMBS(n)                                                       \
+    ((mp_size_t) ((__GMP_MAX (53, n) + 2 * GMP_NUMB_BITS - 1) / GMP_NUMB_BITS))
+#define MPF_D_SIZE(prec) (((prec) + 1) * sizeof(mp_limb_t))
+#define MPF_PACKED_LIMBS(prec) ((prec) + 4);
+#define MPF_PACKED_BYTES ((NLIMBS(mpf_get_default_prec()) + 3) * sizeof(mp_limb_t))
+
+mp_limb_t *pack(mp_limb_t *dest, mpf_t *src, int n)
 {
-    if (dest == NULL)
-        dest = (mp_limb_t *)malloc(PACKED_BYTES * n);
+    if (dest == NULL) {
+        size_t size = 0;
+        for (int i = 0; i < n; i++)
+            size += src[i]->_mp_prec + 4;
+        dest = (mp_limb_t *)malloc(size * sizeof(mp_limb_t));
+    }
     size_t offset = 0;
     for (int i = 0; i < n; i++) {
-        dest[offset + 0] = src[i]->_mp_size;
-        dest[offset + 1] = src[i]->_mp_prec;
+        int prec = dest[offset] = src[i]->_mp_prec;
+        dest[offset + 1] = src[i]->_mp_size;
         dest[offset + 2] = src[i]->_mp_exp;
-        memcpy(&dest[offset + 3], src[i]->_mp_d, NLIMBS * sizeof(mp_limb_t));
-        offset += PACKED_LIMBS;
+        memcpy(&dest[offset + 3], src[i]->_mp_d, MPF_D_SIZE(prec));
+        offset += MPF_PACKED_LIMBS(prec);
     }
     return dest;
 }
 
 mpf_t *unpack(mpf_t *dest, mp_limb_t *src, int n)
 {
-    if (dest == NULL)
+    if (dest == NULL) {
         dest = (mpf_t *)malloc(sizeof(mpf_t) * n);
+    }
+    else {
+        for (int i = 0; i < n; i++)
+            if (dest[i]->_mp_d)
+                free(dest[i]->_mp_d);
+    }
     size_t offset = 0;
     for (int i = 0; i < n; i++) {
-        dest[i]->_mp_size = src[offset + 0];
-        dest[i]->_mp_prec = src[offset + 1];
+        int prec = dest[i]->_mp_prec = src[offset];
+        dest[i]->_mp_size = src[offset + 1];
         dest[i]->_mp_exp = src[offset + 2];
-        dest[i]->_mp_d = malloc(NLIMBS * sizeof(mp_limb_t));
-        memcpy(dest[i]->_mp_d, &src[offset + 3], NLIMBS * sizeof(mp_limb_t));
-        offset += PACKED_LIMBS;
+        dest[i]->_mp_d = malloc((prec + 1) * sizeof(mp_limb_t));
+        memcpy(dest[i]->_mp_d, &src[offset + 3], MPF_D_SIZE(prec));
+        offset += MPF_PACKED_LIMBS(prec);
     }
     return dest;
 }
 
-void packed_add(void *__in, void *__inout, int *len, MPI_Datatype *datatype)
+void packed_add(void *_in, void *_inout, int *len, MPI_Datatype *datatype)
 {
-    mp_limb_t *_in = (mp_limb_t *)__in;
-    mp_limb_t *_inout = (mp_limb_t *)__inout;
-
-    mpf_t *in = unpack(NULL, _in, *len);
-    mpf_t *inout = unpack(NULL, _inout, *len);
+    mpf_t *in = unpack(NULL, (mp_limb_t *)_in, *len);
+    mpf_t *inout = unpack(NULL, (mp_limb_t *)_inout, *len);
 
     for (int i = 0; i < *len; i++)
         mpf_add(inout[i], in[i], inout[i]);
 
-    pack(_inout, inout, *len);
+    pack((mp_limb_t *)_inout, inout, *len);
+
+    for (int i = 0; i < *len; i++) {
+        mpf_clear(in[i]);
+        mpf_clear(inout[i]);
+    }
     free(in);
     free(inout);
 }
@@ -69,7 +83,7 @@ int main(int argc, char **argv)
 
     // Create an MPI Datatype based on the target precision
     MPI_Datatype MPI_MPF;
-    MPI_Type_contiguous(PACKED_BYTES, MPI_CHAR, &MPI_MPF);
+    MPI_Type_contiguous(MPF_PACKED_BYTES, MPI_CHAR, &MPI_MPF);
     MPI_Type_commit(&MPI_MPF);
 
     // Create an MPI Operation for adding mpfs
@@ -83,7 +97,7 @@ int main(int argc, char **argv)
 
     // Pack the number and sum over all processes
     mp_limb_t *packed = pack(NULL, &n, 1);
-    mp_limb_t *sum_packed = malloc(PACKED_BYTES);
+    mp_limb_t *sum_packed = malloc(MPF_PACKED_BYTES);
     MPI_Reduce(packed, sum_packed, 1, MPI_MPF, MPI_SUM_MPF, 0, MPI_COMM_WORLD);
 
     // Unpack the sum and print it
@@ -93,10 +107,12 @@ int main(int argc, char **argv)
         mp_exp_t exp;
         char *repr = mpf_get_str(NULL, &exp, 10, 0, sum);
         printf("value: 0.%s * 10^%d\n", repr, exp);
+        mpf_clear(sum);
     }
 
     free(packed);
     free(sum_packed);
+    mpf_clear(n);
 
     MPI_Finalize();
     return 0;
